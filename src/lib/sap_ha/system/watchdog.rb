b86a700a4e26f96603feb8797f7bd61636a86301
Peter Varkoly <varkoly@suse.com>
2022-05-13 14:09:14 +0200

    yast-sap-ha system/watchdog.rb searches watchdog modules with .ko extension but we ship .ko.xz bsc#1197290

--- src/lib/sap_ha/system/watchdog.rb
+++ src/lib/sap_ha/system/watchdog.rb
@@ -108,11 +108,14 @@ module SapHA
 
       # Get the list of all watchdog available in the system
       def list_watchdogs
+	wmods = []
         unless Dir.exist?(MODULES_PATH)
           log.error "Could not find the kernel modules source directory #{MODULES_PATH}"
           return []
         end
-        Dir.glob(MODULES_PATH + '/*.ko*').map { |path| File.basename(path, '.ko*') }
+	wmods = Dir.glob(MODULES_PATH + '/*.ko').map { |path| File.basename(path, '.ko') }
+        wmods += Dir.glob(MODULES_PATH + '/*.ko.xz').map { |path| File.basename(path, '.ko.xz') }
+        wmods
       end
 
       # Look into the /etc/modules-load.d and list all of the modules
