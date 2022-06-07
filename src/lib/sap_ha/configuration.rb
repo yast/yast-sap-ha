40538c45d8e773a6807c19550f4018beddaa09bd
Peter Varkoly <varkoly@suse.com>
2022-06-07 12:37:18 +0200

    Parameter aliases must be set to true in the new ruby version.

--- src/lib/sap_ha/configuration.rb
+++ src/lib/sap_ha/configuration.rb
@@ -233,7 +233,7 @@ module SapHA
     # Load scenarios from the YAML configuration file
     def load_scenarios
       log.debug "--- called #{self.class}.#{__callee__} ---"
-      YAML.load_file(SapHA::Helpers.data_file_path('scenarios.yaml'))
+      YAML.load_file(SapHA::Helpers.data_file_path('scenarios.yaml'), aliases: true)
     end
   end # class Configuration
 end # module Yast
