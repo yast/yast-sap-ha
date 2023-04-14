500582b626b3a656f666376fe0199f8581d62698
Peter Varkoly <varkoly@suse.com>
2023-04-14 19:03:09 +0200

    Use the same variable

--- src/lib/sap_ha/rpc_server.rb
+++ src/lib/sap_ha/rpc_server.rb
@@ -194,9 +194,10 @@ module SapHA
       @logger.info "--- #{self.class}.#{__callee__} ---"
       out, status = exec_outerr_status('/usr/bin/firewall-cmd', '--status')
       return if status.exitstatus != 0
-      _out, rc  = exec_output_status('/usr/bin/firewall-cmd', '--add-port', '8080/tcp')
+      out, status  = exec_output_status('/usr/bin/firewall-cmd', '--add-port', '8080/tcp')
+      puts "open_port: status=#{status}, out=#{out}"
       @port_opened = true
-      rc.exitstatus == 0
+      status.exitstatus == 0
     end
 
     # close the RPC Server port by manipulating the iptables directly
@@ -204,10 +205,10 @@ module SapHA
       @logger.info "--- #{self.class}.#{__callee__} ---"
       out, status = exec_outerr_status('/usr/bin/firewall-cmd', '--status')
       return if status.exitstatus != 0
-      _out, rc  = exec_output_status('/usr/bin/firewall-cmd', '--remove-port', '8080/tcp')
-      puts "close_port: rc=#{rc}, out=#{out}"
+      out, status  = exec_output_status('/usr/bin/firewall-cmd', '--remove-port', '8080/tcp')
+      puts "close_port: status=#{status}, out=#{out}"
       @port_opened = false
-      rc.exitstatus == 0
+      status.exitstatus == 0
     end
 
   end
