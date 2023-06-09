20b97a5df1dbf12b819f60f858d05fae5a76cdd0
Peter Varkoly <varkoly@suse.com>
2023-06-09 07:47:04 +0200

    New function to get the primary hostname on the master. Fix setting secondary and primary hostname for the template

--- src/lib/sap_ha/configuration/cluster.rb
+++ src/lib/sap_ha/configuration/cluster.rb
@@ -278,6 +278,16 @@ module SapHA
         end.compact
       end
 
+      def get_primary_on_primary
+        SapHA::System::Network.ip_addresses.each do |my_ip|
+	  @nodes.map do |_, node|
+	    if node[:ip_ring1] == my_ip
+	      return node[:host_name]
+	    end
+	  end
+	end
+      end
+
       def validate(verbosity = :verbose)
         validate_comm_layer(verbosity).concat(validate_nodes(verbosity))
       end
